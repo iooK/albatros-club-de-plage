03f2725eb21265010d32d938f40158b8db2e1aa8
sebastien <[omitted]>
2019-07-22 20:32:22 +0200

    modif Equipe

--- src/app/Team/teamData.js
+++ src/app/Team/teamData.js
@@ -3,29 +3,29 @@ const teamData = [{
     'name': 'Goofy',
     'description': 'La mascotte de l Albatros. Cocker anglais. ',
 }, {
-    'image': '/live/img/team2/julie.jpg',
-    'name': 'Julie',
-    'description': 'Animatrice des 3-6 ans, titulaire du Master métier de l\'enseignement, de l\'éducation  et de la formation.',
-}, {
     'image': '/live/img/team2/nicolas.jpg',
     'name': 'Nicolas',
     'description': 'Directeur associé du club de plage, maître nageur diplômé d\'état. Titulaire du Master Staps.',
-}, {
-    'image': '/live/img/team2/melanie.jpg',
-    'name': 'Mélanie',
-    'description': 'Animatrice des 3-6 ans en préparation du diplôme de professeur de Danse. ',
 }, {
     'image': '/live/img/team2/julien.jpg',
     'name': 'Julien',
     'description': 'Directeur associé du club de plage, maître nageur diplômé d\'état. Titulaire du Master Staps.',
 }, {
-    'image': '/live/img/team2/malo.jpg',
-    'name': 'Malo',
-    'description': 'Maître nageur diplôme d état.',
+    'image': '/live/img/team2/alasanne.jpg',
+    'name': 'Alasanne',
+    'description': 'Animateur BAFA',
 }, {
-    'image': '/live/img/team2/marie.jpg',
-    'name': 'Marie',
-    'description': 'Diplômée licence STAPS éducation motricité',
+    'image': '/live/img/team2/augustin.jpg',
+    'name': 'Augustin',
+    'description': 'Animateur BAFA',
+}, {
+    'image': '/live/img/team2/anne.jpg',
+    'name': 'Anne-Claire',
+    'description': 'Professeure des Ecoles',
+}, {
+    'image': '/live/img/team2/thibault.jpg',
+    'name': 'Thibault',
+    'description': 'Animateur BAFA',
 }];
 
 export default teamData;
